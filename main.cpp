a8020df0a196b534c63d29dc69c608fba730c7e5
David Raibaut <d.raibaut@student.holymary.es>
2021-02-27 20:43:01 +0000

    Added debug features and cleaned main up. Math works!

--- main.cpp
+++ main.cpp
@@ -3,22 +3,36 @@
 #include "parser.h"
 #include "interpreter.h"
 
+bool debug = true;
+std::string expression;
 int main() {
-    std::string expression;
-    std::cout << ">>> ";
-    std::getline(std::cin, expression);
-    Lexer lexer = Lexer(expression);
-    std::vector<Token> tokens = lexer.generateTokens();
-    std::cout << "[";
-    for (int i = 0 ; i < tokens.size(); i++) {
-        Token token = tokens[i];
-        if (i == tokens.size()-1)
-            std::cout << token;
-        else
-            std::cout << token << ", ";
+    while (1) {
+        std::cout << ">>> ";
+        std::getline(std::cin, expression);
+
+        Lexer lexer = Lexer(expression);
+        std::vector<Token> tokens = lexer.generateTokens();
+
+        Parser parser = Parser(tokens);
+        Node tree = parser.parse();
+
+        /* DEBUGGING LEXER AND PARSER */
+        if (debug) {
+            std::cout << "[";
+            for (int i = 0 ; i < tokens.size(); i++) {
+                Token token = tokens[i];
+                if (i == tokens.size()-1)
+                    std::cout << token;
+                else
+                    std::cout << token << ", ";
+            }
+            std::cout << "]\n";
+            std::cout << "AST: " << tree << "\n";
+        }
+        
+        Interpreter interpreter = Interpreter();
+        Number result = interpreter.visit(tree);
+
+        std::cout << result << "\n";
     }
-    std::cout << "]\n";
-    Parser parser = Parser(tokens);
-    Node tree = parser.parse();
-    std::cout << "AST: " << tree << "\n";
 }
