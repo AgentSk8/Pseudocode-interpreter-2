80df5b507d5f85dad56f934d330a3059c0e4f595
David Raibaut <>
2021-04-13 10:49:44 +0000

    Small python program to output the number of lines, files and characters with .h or .cpp extension out of curiosity.

---
+++ countlines.py
@@ -0,0 +1,12 @@
+import os
+files = 0
+lines = 0
+characters = 0
+for file in os.listdir('.'):
+	ext = file.split('.')[-1]
+	if  ext == "cpp" or ext == "h":
+		f = open(file,'r').read()
+		lines += len(f.split('\n'))
+		characters += len(f)
+		files += 1
+print("CPP or H files\n"+"="*(12+len(str(characters)))+"\nFiles: %i\nLines: %i\nCharacters: %i\n" % (files, lines, characters))
